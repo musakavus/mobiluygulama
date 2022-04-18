e0372b88f7c70c1acc5191d7853ba31322ea4a1d
musakavus <musakavus@gmail.com>
2022-04-19 01:20:48 +0300

    git push origin master

--- app/src/main/java/com/example/yalovafood/MainActivity.kt
+++ app/src/main/java/com/example/yalovafood/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.yalovafood
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+    private fun validateName(): Boolean? {
+        val `val`: String = inputUsername.getEditText().getText().toString()
+        return if (`val`.isEmpty()) {
+            regName.setError("Field cannot be empty")
+            false
+        } else {
+            regName.setError(null)
+            regName.setErrorEnabled(false)
+            true
+        }
     }
 }
