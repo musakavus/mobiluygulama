78d8f5fb7031b3e9a2ccdef6585e128488bf398f
musakavus <musakavus@gmail.com>
2022-04-24 02:29:51 +0300

    git push origin master

--- app/src/main/java/com/example/yalovafood/MainActivity.kt
+++ app/src/main/java/com/example/yalovafood/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.yalovafood
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val secondButton = findViewById<Button>(R.id.btnRegister)
+        secondButton.setOnClickListener{
+            val intent2 = Intent(this, OrderActivity::class.java)
+            startActivity(intent2)
+        }
+
+
     }
 }
