0ea9c59528d0168a771c16afa764442752f1cae7
musakavus <musakavus@gmail.com>
2022-04-24 00:44:13 +0300

    git push origin master

--- app/src/main/java/com/example/yalovafood/MainActivity.kt
+++ app/src/main/java/com/example/yalovafood/MainActivity.kt
@@ -10,15 +10,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
     }
-    private fun validateName(): Boolean? {
-        val `val`: String = inputUsername.getEditText().getText().toString()
-        return if (`val`.isEmpty()) {
-            regName.setError("Field cannot be empty")
-            false
-        } else {
-            regName.setError(null)
-            regName.setErrorEnabled(false)
-            true
-        }
-    }
 }
